7cea8f08ec77df99eb0dde6db75c46cbe142d126
ajalewis <alexanderashworthlewis@gmail.com>
2022-01-17 14:40:26 +1000

    Adjust script to include additional cmds

--- scripts/setup-hosts.sh
+++ scripts/setup-hosts.sh
@@ -20,3 +20,51 @@ sudo timedatectl set-timezone 'Australia/Brisbane'
 # Remove unneccessary MOTD addons
 sudo chmod -x /etc/update-motd.d/00-header /etc/update-motd.d/10-help-text
 
+# Set iptables to bridged
+
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
+# Install docker run time
+
+sudo apt-get update && sudo apt-get install ca-certificates curl nupg lsb-release apt-transport-https -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Configure docker daemon
+
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+# Install kubeadm kubectl kubelet
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+sudo apt-get update && sudo apt-get install kubelet kubeadm kubectl -y && sudo apt-mark hold kubelet kubeadm kubectl
